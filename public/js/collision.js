7edcc3015828e7c7967fbbb76d639fe8e517cce9
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-11-15 22:19:02 -0300

    [add] collision.js detecting collision

--- public/js/collision.js
+++ public/js/collision.js
@@ -1,3 +1,23 @@
-function collision(ball, player){
-    console.log(ball, player);
+function collision(b, p){
+
+    p.top = p.y
+    p.bottom = p.y + p.height
+    p.left = p.x
+    p.right = p.x + p.width
+
+    
+    b.top = b.y
+    b.bottom = b.y + b.height
+    b.left = b.x
+    b.right = b.x + b.width
+    
+
+    // if((b.right > p.left) && (b.top < p.bottom) && (b.left < p.right) && (b.bottom > p.top)){
+    //     console.log("COLLISION!");
+    // }
+
+    return b.right > p.left && b.top < p.bottom && b.left < p.right && b.bottom > p.top
 }
+
+
+
