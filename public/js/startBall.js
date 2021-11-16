d78cb33be38dd3cc2b6bc51a32225441f3beb5fd
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-11-16 00:28:06 -0300

    [changed] index.html bindings

--- public/js/startBall.js
+++ public/js/startBall.js
@@ -1,12 +1,12 @@
 const min = -10
 
-function startBallPosition(app){
-    app.getNode('red-box').x = app.canvas.width / 2
-    app.getNode('red-box').y = app.canvas.height / 2 - 100 / 2
+function startBallPosition(){
+    this.getNode('red-box').x = this.canvas.width / 2
+    this.getNode('red-box').y = this.canvas.height / 2 - 100 / 2
 
     let randomNumber = Math.random() * (10 - min) + min
 
-    app.getNode('red-box').directionX = randomNumber;
-    app.getNode('red-box').directionY = -randomNumber;
+    this.getNode('red-box').directionX = randomNumber;
+    this.getNode('red-box').directionY = -randomNumber;
 }
 
