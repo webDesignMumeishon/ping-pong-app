51f0d92ac1bf4da773c19bdbfe4280deff7ba3c7
Tomas Ignacio Macchi <73501650+webDesignMumeishon@users.noreply.github.com>
2021-11-17 11:13:41 -0300

    Revert "Tomas dev"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port    = 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.send('Welcome to the test!')
+	res.send('Welcome!')
 });
 
 app.listen(port, () => {
--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,8 @@ var app = {
 	context : null,
 
 	//resizing
-	width   : 1300,
-	height  : 500,
-
-	//id for stopping the animation
-	id : undefined,
+	width   : 800,
+	height  : 400,
 
 	//nodes
 	nodes   : [],
@@ -21,56 +18,34 @@ var app = {
 	init : function(){
 		this.canvas  = document.getElementById('canvas');
 		this.context = this.canvas.getContext('2d');
+
 		this.render();
 		this.onInit();
-
 	},
 	render : function(){
 		this.clear();
 		this.update();
-		this.id = window.requestAnimationFrame(this.render.bind(this));
+
+		window.requestAnimationFrame(this.render.bind(this));
 	},
 	clear  : function(){
 		this.context.clearRect(0, 0, this.width, this.height);
 	},
 	update : function(){
 	    var dt = Date.now() - this.lastUpdate;
+
 		this.onUpdate(dt);
 
 		for(var index in this.nodes){
 			var node = this.nodes[index];
 
-			if(node.text >= 0){
-				this.drawText(node)
-			}
-			else if(node.id === "red-box"){
-				this.drawCircle(node)
-			}
-			else{
-				this.context.fillStyle = node.color;
-				this.context.fillRect(node.x, node.y, node.width, node.height);
-			}
+			this.context.fillStyle = node.color;
+			this.context.fillRect(node.x, node.y, node.width, node.height);
 		}
+
 		this.lastUpdate = Date.now();
 		this.timestamp+=dt;
 	},
-
-	drawText: function(node){
-		this.context.fillStyle = node.color;
-		this.context.fillText(node.text, node.x, node.y)
-		this.context.font = "75px Arial"
-	},
-
-	drawCircle: function(node){
-		this.context.fillStyle = node.color;
-		this.context.beginPath()
-		this.context.arc(node.x, node.y,node.r, 0, Math.PI*2, false)
-		this.context.closePath()
-		this.context.fill()
-	},
-
-
-
 	getNode : function(id){
 		for(var index in this.nodes){
 			var node = this.nodes[index];
@@ -79,6 +54,7 @@ var app = {
 				return node;
 			}
 		}
+
 		return { x : null, y : null, width : null, height : null };
 	},
 
