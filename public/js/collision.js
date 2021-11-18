94860d8774a426aa899a5e6124b6f9e6f9ef0436
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-11-18 08:28:25 -0300

    [change] variable naming collision.js

--- public/js/collision.js
+++ public/js/collision.js
@@ -1,16 +1,16 @@
-export function collision(b, p){
+export function collision(ball, playerPaddle){
 
-    p.top = p.y
-    p.bottom = p.y + p.height
-    p.left = p.x
-    p.right = p.x + p.width
+    playerPaddle.top = playerPaddle.y
+    playerPaddle.bottom = playerPaddle.y + playerPaddle.height
+    playerPaddle.left = playerPaddle.x
+    playerPaddle.right = playerPaddle.x + playerPaddle.width
     
-    b.top = b.y - b.r
-    b.bottom = b.y + b.r
-    b.left = b.x - b.r
-    b.right = b.x + b.r
+    ball.top = ball.y - ball.r
+    ball.bottom = ball.y + ball.r
+    ball.left = ball.x - ball.r
+    ball.right = ball.x + ball.r
 
-    return b.right > p.left && b.top < p.bottom && b.left < p.right && b.bottom > p.top
+    return ball.right > playerPaddle.left && ball.top < playerPaddle.bottom && ball.left < playerPaddle.right && ball.bottom > playerPaddle.top
 }
 
 
