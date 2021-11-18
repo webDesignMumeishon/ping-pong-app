82a88f4e3d1f6de14e0a3af76dfa5ed7b047620d
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-11-18 11:10:01 -0300

    [remove] console.log start.js

--- public/js/start.js
+++ public/js/start.js
@@ -2,6 +2,9 @@ const MIN_STARTING_POINT = -10;
 const MAX_STARTING_POINT = 10;
 const INIT_BALL_SPEED = 15;
 const INCREMENT_RANGE = 5
+const OUT_RANGE_ONE = -2
+const OUT_RANGE_TWO = 2
+
 
 
 export function startBallPosition(app){
@@ -17,7 +20,5 @@ export function startBallPosition(app){
     ball.speed = INIT_BALL_SPEED
     ball.directionX = checkIfBallInitialDirectionIsOutOfRange ? ballInitialDirectionX + INCREMENT_RANGE : ballInitialDirectionX;
     ball.directionY = ballInitialDirectionY
-
-    console.log(ball.directionX, ball.directionY);
 }
 
