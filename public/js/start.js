2136ba999b87686e7b17968f5ce1b806f44a9565
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-11-18 17:54:03 -0300

    [refactoring] renaming variables

--- public/js/start.js
+++ public/js/start.js
@@ -4,6 +4,7 @@ const INIT_BALL_SPEED = 15;
 const INCREMENT_RANGE = 5
 const OUT_RANGE_ONE = -2
 const OUT_RANGE_TWO = 2
+const BALL_SIZE = 50
 
 
 export function startBallPosition(app){
@@ -12,12 +13,12 @@ export function startBallPosition(app){
 
     const ball = app.getNode("red-box")
     ball.x = MIDDLE_OF_COURT_WIDTH
-    ball.y = MIDDLE_OF_COURT_HEIGHT - 100 / 2
+    ball.y = MIDDLE_OF_COURT_HEIGHT - BALL_SIZE / 2
 
     let ballInitialDirectionX = Math.random() * (MAX_STARTING_POINT - MIN_STARTING_POINT) + MIN_STARTING_POINT 
     let ballInitialDirectionY = Math.random() * (MAX_STARTING_POINT - MIN_STARTING_POINT) + MIN_STARTING_POINT 
 
-    let checkIfBallInitialDirectionIsOutOfRange = ballInitialDirectionX > -2 && ballInitialDirectionX < 2
+    let checkIfBallInitialDirectionIsOutOfRange = ballInitialDirectionX > OUT_RANGE_ONE && ballInitialDirectionX < OUT_RANGE_TWO
 
     ball.speed = INIT_BALL_SPEED
     ball.directionX = checkIfBallInitialDirectionIsOutOfRange ? ballInitialDirectionX + INCREMENT_RANGE : ballInitialDirectionX;
