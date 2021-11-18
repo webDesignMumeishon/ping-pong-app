770719672076234acae6f58d7269a0050a39177c
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-11-18 18:08:40 -0300

    [refactoring] renaming variables

--- public/js/resize.js
+++ public/js/resize.js
@@ -1,6 +1,8 @@
-const playerPaddleWidth = 50
 import {app} from './app.js'
-const MARGIN_BOTTOM_HEIGHT = 5
+
+const MARGIN_BOTTOM_HEIGHT = 5  
+const PLAYER_PADDLE_WIDTH = 50
+
 export function resize(e){
     let playerTwoPosition = app.getNode("player-two")
     let canvas = document.getElementById('canvas')
@@ -16,12 +18,16 @@ export function resize(e){
     app.width = width
     app.height = heigth - MARGIN_BOTTOM_HEIGHT
 
-	playerTwoPosition.x = app.width - playerPaddleWidth
+	playerTwoPosition.x = app.width - PLAYER_PADDLE_WIDTH
 
-    playerOneScorePosition.x = app.width / 4
-    playerOneScorePosition.y = app.height / 5
+    const scoreLocationVartically = app.height / 5
+    const scoreLocationPlayerOneHorizontally = app.width / 4
+    const scoreLocationPlayerTwoHorizontally = 3 * app.width / 4
 
-    playerTwoScorePosition.x = 3 * app.width / 4
-    playerTwoScorePosition.y = app.height / 5
+    playerOneScorePosition.x = scoreLocationPlayerOneHorizontally
+    playerOneScorePosition.y = scoreLocationVartically
+
+    playerTwoScorePosition.x = scoreLocationPlayerTwoHorizontally
+    playerTwoScorePosition.y = scoreLocationVartically
 
 }
